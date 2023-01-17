e7b487192cbc9158360e58c2d01c5933d71802ac
Alan Bobs <amna25499@gmail.com>
2023-01-17 17:21:38 +0800

    feat: add onChanged() for triggering update

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
 import '@logseq/libs';
 
 //Inputs 5 numbered blocks when called
-async function insertSomeBlocks (e) {
-  console.log('Open the calendar!')
-  let numberArray = [1, 2, 3, 4, 5]
-  for (const number in numberArray){
-  logseq.App.showMsg("Function has been run")
-  logseq.Editor.insertBlock(e.uuid, `This is block ${numberArray[number]}`, {sibling: true})}
+// async function insertSomeBlocks (e) {
+//   console.log('Open the calendar!')
+//   let numberArray = [1, 2, 3, 4, 5]
+//   for (const number in numberArray){
+//   logseq.App.showMsg("Function has been run")
+//   logseq.Editor.insertBlock(e.uuid, `This is block ${numberArray[number]}`, {sibling: true})}
 
-  }
+//   }
+
+//TODO get parent blocks and child block todo status
+//getBlockProperties
+async function getWorkTree (currentBlockUuid) {
+  return;
+}
+
+//TODO specify conditions
+//TODO update status method
+async function updateStatus (currentBlockUuid,status) {
+  const block = await logseq.Editor.getBlock(currentBlockUuid);
+  const workTree = await getWorkTree(currentBlockUuid);
+  if (block.marker === status.todo) {
+    console.log(`Get block marker: ${block.marker}`);
+    let blockContent = block.content.slice(block.content.indexOf(' '));
+    await logseq.Editor.updateBlock(currentBlockUuid, status.done + blockContent);
+    logseq.UI.showMsg(`${block.marker}`);
+  };
+}
 
 const main = async () => {
-  console.log('plugin loaded');
-  logseq.Editor.registerSlashCommand('insertBlocks', async (e) => {
-    insertSomeBlocks(e)
-  }
-    
-  )}
+  console.log('Init automatic done service.')
+
+  const config = await logseq.App.getUserConfigs();
+  const preferredWorkflow = config.preferredWorkflow;
+
+  let status = {};
+  if (preferredWorkflow === 'now') {
+      status = {todo: 'LATER',doing: 'NOW',done: 'DONE'};
+    } else {
+      status = {todo: 'TODO',doing: 'DOING',done: 'DONE'};
+    }
+
+  logseq.DB.onChanged(async () => {
+    const block = await logseq.Editor.getCurrentBlock();
+    let currentBlockUuid = block.uuid;
+
+    updateStatus(currentBlockUuid,status);
+  });
+};
 
 logseq.ready(main).catch(console.error);
